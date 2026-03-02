7003b70e8eef8f5fd01b3550636db0c5c5accee8
francesco.marcatelli <francesco.marcatelli@studio.unibo.it>
2026-03-02 12:02:00 +0100

    Started es2 with tests

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -1,15 +1,40 @@
 package it.unibo.pps.e2;
 import org.junit.jupiter.api.*;
 
-public class LogicTest {
+import java.util.stream.IntStream;
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LogicTest {
+    Logics logic;
+    private final int SIZE = 5;
+    @BeforeEach
+    public void init() {
+        logic = new LogicsImpl(SIZE);
+    }
+    @Test
+    public void testPawnInBoard() {
+        boolean flagPawn = false;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (this.logic.hasPawn(i, j)) {
+                    flagPawn = true;
+                }
+            }
+        }
+        assertTrue(flagPawn);
+    }
+
+    @Test
+    public void testKnightInBoard() {
+        boolean flagKnight = false;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (this.logic.hasPawn(i, j)) {
+                    flagKnight = true;
+                }
+            }
+        }
+        assertTrue(flagKnight);
+    }
 }
